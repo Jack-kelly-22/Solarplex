db309b00a97e282d4867adde7c653b877a58d2f7
Opto_nuke <90173621+opto-nuke@users.noreply.github.com>
2021-10-16 14:47:25 -0700

    fix name for jar file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #setup computer for use with the project
 #install sshd and change port
+sudo apt install git
 sudo apt install sshd
 sudo apt install network-tools
 sudo systemctl enable ssh
 sudo systemctl start ssh
+sudo apt install openjdk-17-jre-headless
 sudo ufw allow ssh
-git clone 
+git clone git@github.com:Jack-kelly-22/Solarplex.git
+cd Solarplex
+sudo apt install python3-pip
+
+
