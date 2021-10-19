3490395a5536cf3306a801e09c067dfd2c8ebf51
Opto_nuke <90173621+opto-nuke@users.noreply.github.com>
2021-10-19 13:17:04 -0700

    remote changes merge

--- solarplex-backend/admin.py
+++ solarplex-backend/admin.py
@@ -1,13 +1,10 @@
 
 import os
+import threading
 #a file to manage the collection of all the admin functions
-def ping_host(ip_address):
-    response = os.system("ping -c 1 " + ip_address)
-    if response == 0:
-        return True
-    else:
-        print(response)
-        return False
+
+t = os.system("ip a")
+print(t)
 
 
 
